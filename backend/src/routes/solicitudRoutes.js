b843fbb91ebcdc191a9ca36c016d5721f4b9b69d
EnzoAyala94 <enzoantoniomartinezayala@gmail.com>
2026-08-30 23:21:40 -0600

    Update solicitudRoutes.js

--- backend/src/routes/solicitudRoutes.js
+++ backend/src/routes/solicitudRoutes.js
@@ -6,6 +6,6 @@ const { listarSolicitudes, responderSolicitud } = require('../controllers/solici
 router.use(verificarToken);
 
 router.get('/', listarSolicitudes);
-router.put('/:id', requiereRol('admin', 'facturador', 'contador'), responderSolicitud);
+router.put('/:id', requiereRol('admin', 'superadmin', 'facturador', 'contador'), responderSolicitud);
 
 module.exports = router;
